8c26711f24d8c388063b9974d2a176fa90003cde
Vincent Barilla <vbarilla@gmail.com>
2017-09-10 00:25:25 -0700

    Fix collapsing for link, add italics for movie title

--- front-end/movies-evan-likes/src/components/MovieListItem.js
+++ front-end/movies-evan-likes/src/components/MovieListItem.js
@@ -14,16 +14,18 @@ export default class MovieListItem extends Component {
   }
 
   toggleContentCollapse(event) {
-    this.setState({
-      contentClassName: this.state.contentClassName === 'list-item-content' ? 'collapse' : 'list-item-content'
-    })
+    if (!event.target.classList.contains('do-not-collapse')) {
+      this.setState({
+        contentClassName: this.state.contentClassName === 'list-item-content' ? 'collapse' : 'list-item-content'
+      })
+    }
   }
 
   render() {
     return (
       <li className={`movie-list-item ${this.alternateDarkness()}`} onClick={this.toggleContentCollapse.bind(this)}>
-        <a href={this.movie.url}>
-          {Math.round(this.movie.score * 100)}% {this.movie.title} ({this.movie.year})
+        <a className='do-not-collapse' href={this.movie.url}>
+          {Math.round(this.movie.score * 100)}% <i className='do-not-collapse'>{this.movie.title}</i> ({this.movie.year})
         </a>
         <div className={this.state.contentClassName}>
           <div className='movie-info'>
